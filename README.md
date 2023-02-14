d4e545d85f5029999c10bc40e316a0d55499ec96
SahityaUniyal <sahityauniyal.com>
2023-02-14 13:04:58 +0530

    change style in which the message is displayed

--- README.md
+++ README.md
@@ -1,14 +1,18 @@
 # RealTimeChatApp
 
 ## How to use the chat app
-    - Download the client and chat folder from git.
-    - Run the app.js file using node in server folder and keep it running.
-    - Now open the client folder and run home.html file to join the chat group.
-    - You can open as many client files as you want.
+- Download the client and chat folder from git.
+- Run the app.js file using node in server folder and keep it running.
+- Now open the client folder and run home.html file to join the chat group.
+- You can open as many client files as you want.
 
-## Working
-    - This app is built using socket.io api.
-    - The client side is static website which links to the server using JavaScript.
-    - The server runs using node and the server need to keep running so that the client can connect to the server.
+## Technology Stack
+- HTML
+- CSS
+- JavaScript
+- Node.js
+- Socket.io
 
-<img src="image.png">
+## About
+- This app is built using socket.io api.
+- The app lets any number of users join and chat among themselves.
--- client/client.js
+++ client/client.js
@@ -6,11 +6,18 @@ const messageContainer = document.querySelector(".messages-container");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 
 // Function to add element to the message container
-const append= (message,pos)=>{
+const append= (name,message,pos)=>{
+    const messageElement= document.createElement("div")
+    messageElement.innerHTML="<p class='name'>"+name+"</p>"+"<p class='msg'>"+message+"</p>";
+    messageElement.classList.add(pos);
+    messageContainer.append(messageElement);
+};
+const appendJoin=(message,pos)=>{
     const messageElement= document.createElement("p")
     messageElement.innerHTML=message;
     messageElement.classList.add(pos);
     messageContainer.append(messageElement);
+
 };
 
 const appendMembers=(userName)=>{
@@ -36,7 +43,7 @@ socket.on("old-users",(data)=>{
 });
 
 // notification for joining 
-append(`Welcome to the chat group ${yourName}`,"center");
+appendJoin(`Welcome to the chat group ${yourName}`,"center");
 
 //add yourself in the chat members 
 appendMembers(yourName);
@@ -53,7 +60,7 @@ form.addEventListener("submit",(e)=>{
     messageInput.value="";
 
     // adding the message for yourself
-    append(`You: ${message}`,"right");
+    append('You',`${message}`,"right");
 
     // emitting message to other users
     socket.emit("send",message);
@@ -62,18 +69,18 @@ form.addEventListener("submit",(e)=>{
 
 // If another user joins the chat
 socket.on("user-joined",(name)=>{
-    append(`${name} joined the chat`,"center");
+    appendJoin(`${name} joined the chat`,"center");
     appendMembers(name);
 })
 
 // If a message is received form socket.io broadcast
 socket.on("receive",(data)=>{
-    append(`${data.name}: ${data.message}`,"left");
+    append(`${data.name}`,`${data.message}`,"left");
 });
 
 // If a user leaves the chat
 socket.on("left-chat",(name)=>{
-    append(`${name} left the chat`,"center");
+    appendJoin(`${name} left the chat`,"center");
 });
 
 // updates the users if a user leaves the chat
